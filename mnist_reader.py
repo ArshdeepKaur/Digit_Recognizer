4b20849207d7df840c1bc5186608958850afeb46
Arshdeep Kaur <arshi.ihsra@gmail.com>
2018-01-30 21:00:22 -0800

    Added logic for taking out 1st N images for each label & implemented a func that randomly chooses N images for each label

--- mnist_reader.py
+++ mnist_reader.py
@@ -7,6 +7,10 @@ from mnist import MNIST
 import numpy as np
 import time
 import matplotlib.pyplot as plt
+import random
+
+from pandas import DataFrame
+import pandas as pd
 
 #Use the following to isntall this MNIST Reading Library
 #pip install python-mnist
@@ -20,8 +24,6 @@ num_images = 10
 #Waiting Time in Seconds between each displayed image
 wait_time = 0.5
 
-#print("hello world!") 
-
 print ("Images Structure: ", np.shape(training_data[0]))
 print ("Label Structure: ", np.shape(training_data[1]))
 
@@ -32,8 +34,8 @@ Function to Display Images
 def show_images():
 	im_count=0
 	while im_count<num_images:
-		img = np.array(a[0][im_count]) 		#img is 1 x 784
-		label = np.array(a[1][im_count])	#label for img
+		img = np.array(training_data[0][im_count]) 		#img is 1 x 784
+		label = np.array(training_data[1][im_count])	#label for img
 		np.resize(img,(28,28))				#1 x 784 -> 28 x 28
 		img = np.reshape(img, (28, 28))
 		plt.imshow(img, cmap='gray', interpolation='nearest')
@@ -53,3 +55,31 @@ print ("NO. of Input Neurons:", num_input_neurons)
 
 #Function Call to Plot Each Digit Image
 show_images()
+
+
+im_count=0
+training_data_by_labels = [[]] * 10 #lists of 10 lists
+
+while im_count<len(training_data[1]):
+	label = np.array(training_data[1][im_count])	#extract label for image
+	training_data_by_labels[label].append(np.array(training_data[0][im_count])) 
+	im_count+=1
+
+'''
+Function to pick n random images from each label
+'''
+def  randomly_choose_n_images(n): # inputs: n (number of images)
+	random_training_data_by_labels = [[]] * 10 #lists of 10 lists
+	random_values = []
+	random_values = range(1, len(training_data_by_labels[label]), n)
+	itr = 0
+	label_count = 0
+	while (label_count < 10):
+    		while (itr < len(random_values)):
+	    		random_training_data_by_labels[label].append(training_data_by_labels[label][itr])
+	    		itr+=1
+	    	label_count+=1
+	return random_training_data_by_labels
+	
+random_training_data_by_labels = randomly_choose_n_images(20)
+	    
